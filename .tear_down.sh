57b523c61d28ade476438ffab2887547a517708a
james rutledge <jarutledge2@gmail.com>
2022-02-15 21:21:01 -0600

    add .tear_down.sh

---
+++ .tear_down.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+rm -rf ~/.archey.zsh
+rm -rf ~/.bash_prompt.zsh
+rm -rf ~/.git_completion.zsh
+rm -rf ~/.utilities.sh
+rm -rf ~/.zlogin
+rm -rf ~/.zlogout
+rm -rf ~/.zprofile
+rm -rf ~/.zshenv
+rm -rf ~/.zshrc
